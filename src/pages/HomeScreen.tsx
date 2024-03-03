fd310e3286f46efc47eb7fa6dcf5fd62a775f41a
Gaëtan H <gaetanhus@gmail.com>
2024-03-03 23:17:14 +0100

    Refactor HomeScreen component and add check cards

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,11 +1,21 @@
 import type { ReactElement } from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { useSession } from "../lib/hooks/useSession";
+import type { RootStackParamList } from "../../App";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { CheckCard } from "../lib/components/checks/CheckCard";
+import { WeeklyCheckCard } from "../lib/components/checks/WeeklyCheckCard";
 
-export const HomeScreen = (): ReactElement => {
+type Props = NativeStackScreenProps<RootStackParamList>;
+
+export const HomeScreen = ({ navigation }: Props): ReactElement => {
+  const { session } = useSession();
+  if (!session) navigation.navigate("LoginScreen");
+
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={{ padding: 15, gap: 10 }}>
+      <CheckCard />
+      <WeeklyCheckCard />
     </View>
   );
 };
