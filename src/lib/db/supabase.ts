27607d964d51a9b2c044eae3f20c94b209b81328
Gaëtan H <gaetanhus@gmail.com>
2024-03-03 23:14:39 +0100

    Add type definition for Database in supabase.ts

--- src/lib/db/supabase.ts
+++ src/lib/db/supabase.ts
@@ -2,12 +2,13 @@
 import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "./supabase.types";
 
 const SUPABASE_URL = "https://xjgmgvvxdtpgkymqpvnz.supabase.co";
 // eslint-disable-next-line max-len
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqZ21ndnZ4ZHRwZ2t5bXFwdm56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkzOTcwNzEsImV4cCI6MjAyNDk3MzA3MX0.Y_hYQ8S-AXyD3MW3pouna6Df5vz9hTKd7u75lxUCcHs";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
