fcfe676ab1821f5000e5004550dfc97acfe84b0b
Gaëtan H <gaetanhus@gmail.com>
2024-03-03 16:29:27 +0100

    Refactor AppBar component

--- src/lib/components/appbar.tsx
+++ src/lib/components/appbar.tsx
@@ -1,21 +1,18 @@
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { useState, type ReactElement } from "react";
+import type { ReactElement } from "react";
 import { Image } from "react-native";
-import { Appbar, Avatar, Menu } from "react-native-paper";
+import { Appbar } from "react-native-paper";
 import type { RootStackParamList } from "../../../App";
 import { useSession } from "../hooks/useSession";
-import { getAvatar } from "../dicebear";
 
 type Props = NativeStackScreenProps<RootStackParamList>;
 
 export const AppBar = ({ navigation, route }: Props): ReactElement => {
-  const { logout, session } = useSession();
-  const [menuVisible, setMenuVisible] = useState(false);
+  const { session } = useSession();
 
   if (!session && route.name === "LoginScreen" || !session && route.name === "RegisterScreen") {
     return (
       <Appbar>
-        {navigation.canGoBack() && <Appbar.BackAction onPress={navigation.goBack} />}
         {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
         <Image source={require("./assets/logo.jpg")} style={{ width: 100, height: 40 }} />
         <Appbar.Content title="" />
@@ -30,30 +27,16 @@ export const AppBar = ({ navigation, route }: Props): ReactElement => {
       <Image source={require("./assets/logo.jpg")} style={{ width: 100, height: 40 }} />
       <Appbar.Content title="" />
 
-      <Appbar.Action icon="numeric" onPress={() => {
-        console.log("Pressed numeric");
-        navigation.navigate("CounterScreen");
-      }}/>
+      <Appbar.Action icon="numeric" onPress={() => navigation.navigate("CounterScreen")} />
+      <Appbar.Action icon="calendar-month" onPress={() => navigation.navigate("CalendarScreen")} />
 
-      <Appbar.Action icon="calendar-month" onPress={() => {
-        console.log("Pressed calendar");
-        navigation.navigate("CalendarScreen");
-      }}/>
-
-      <Menu visible={menuVisible} onDismiss={() => setMenuVisible(false)}
-        anchor={<Avatar.Image source={{ uri: getAvatar(session?.user.email) }} size={40} />}>
-        <Menu.Item onPress={() => {
-          console.log("Pressed account");
-          navigation.navigate(session ? "SessionScreen" : "LoginScreen");
-        }} title={session ? "Account" : "Login"} trailingIcon={session ? "account" : "account-key"} />
-
-        {session && <Menu.Item onPress={() => {
-          console.log("Pressed logout");
-          logout()
-            .then(() => navigation.navigate("LoginScreen"))
-            .catch((error) => console.error("Error:", error));
-        }} title="Logout" trailingIcon={"logout"} />}
-      </Menu>
+      <Appbar.Action icon={session ? "account" : "account-key"} disabled={
+        !session && route.name === "LoginScreen"
+        || session && route.name === "SessionScreen"
+        || !session && route.name === "RegisterScreen"
+      } onPress={() => {
+        navigation.push(session ? "SessionScreen" : "LoginScreen");
+      }} />
     </Appbar>
   );
 };
