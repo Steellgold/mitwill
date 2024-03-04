6c05d5542935af864f2dc5ae591f608929afa241
Gaëtan H <gaetanhus@gmail.com>
2024-03-04 01:38:40 +0100

    Update AppBar component with additional functionality

--- src/lib/components/appbar.tsx
+++ src/lib/components/appbar.tsx
@@ -1,7 +1,7 @@
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import type { ReactElement } from "react";
 import { Image } from "react-native";
-import { Appbar } from "react-native-paper";
+import { Appbar, Text } from "react-native-paper";
 import type { RootStackParamList } from "../../../App";
 import { useSession } from "../hooks/useSession";
 
@@ -23,12 +23,17 @@ export const AppBar = ({ navigation, route }: Props): ReactElement => {
   return (
     <Appbar>
       {navigation.canGoBack() && <Appbar.BackAction onPress={navigation.goBack} />}
+      {!navigation.canGoBack() && <Text>{"    "}</Text>}
       {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
       <Image source={require("./assets/logo.jpg")} style={{ width: 100, height: 40 }} />
       <Appbar.Content title="" />
 
-      <Appbar.Action icon="numeric" onPress={() => navigation.navigate("CounterScreen")} />
-      <Appbar.Action icon="calendar-month" onPress={() => navigation.navigate("CalendarScreen")} disabled />
+      <Appbar.Action icon="numeric" onPress={() => {
+        navigation.popToTop();
+        navigation.push("CounterScreen");
+      }} />
+
+      <Appbar.Action icon="calendar-month" onPress={() => navigation.push("CalendarScreen")} disabled />
 
       <Appbar.Action icon={session ? "account" : "account-key"} disabled={
         !session && route.name === "LoginScreen"
