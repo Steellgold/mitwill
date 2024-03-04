94c9a24d21890e020491866287a3cd896f5175ab
Gaëtan H <gaetanhus@gmail.com>
2024-03-04 23:26:40 +0100

    Add CheckFAB component to HomeScreen

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -5,17 +5,22 @@ import type { RootStackParamList } from "../../App";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { CheckCard } from "../lib/components/checks/CheckCard";
 import { WeeklyCheckCard } from "../lib/components/checks/WeeklyCheckCard";
+import { CheckFAB } from "../lib/components/checks/CheckFAB";
+import { useIsFocused } from "@react-navigation/native";
 
 type Props = NativeStackScreenProps<RootStackParamList>;
 
 export const HomeScreen = ({ navigation }: Props): ReactElement => {
   const { session } = useSession();
   if (!session) navigation.navigate("LoginScreen");
+  const isFocused = useIsFocused();
 
   return (
     <View style={{ padding: 15, gap: 10 }}>
       <CheckCard />
       <WeeklyCheckCard />
+
+      <CheckFAB visible={isFocused} />
     </View>
   );
 };
