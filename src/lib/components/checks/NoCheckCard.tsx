a2a30112a6e799fb9f0cee1f42e31ee0f9906f38
Gaëtan H <gaetanhus@gmail.com>
2024-03-05 17:35:23 +0100

    Add support for daily-already-ended type in NoCheckCard component

--- src/lib/components/checks/NoCheckCard.tsx
+++ src/lib/components/checks/NoCheckCard.tsx
@@ -4,7 +4,7 @@ import { dayJS } from "../../dayjs/day-js";
 import { majdate } from "../../dayjs/day-js.utils";
 
 type Props = {
-  type: "daily" | "weekly" | "session";
+  type: "daily" | "weekly" | "session" | "daily-already-ended";
 };
 
 export const NoCheckCard = ({ type = "daily" }: Props): ReactElement => {
@@ -13,17 +13,21 @@ export const NoCheckCard = ({ type = "daily" }: Props): ReactElement => {
       <Card.Title
         title={
           type === "daily"
-            ? majdate(dayJS().toDate())
-            : type === "weekly"
-              ? `Semaine du ${dayJS().startOf("week").format("DD MMMM")} au ${dayJS().endOf("week").format("DD MMMM")}`
-              : "Session non détectée"
+            ? majdate(dayJS().toDate()) : type === "daily-already-ended"
+              ? "Journée terminée"
+
+              : type === "weekly"
+                ? `Semaine du ${dayJS().startOf("week").format("DD MMMM")} au ${dayJS().endOf("week").format("DD MMMM")}`
+                : "Session non détectée"
         }
         subtitle={
           type === "daily"
             ? "Vous n'avez pas encore commencé à travailler aujourd'hui ou vous n'avez pas encore pointé votre arrivée"
-            : type === "weekly"
-              ? "Vous n'avez pas encore pointé votre arrivée cette semaine, pensez à le faire tous les jours"
-              : "La session n'est pas détectée, redémarrez l'application ou reconnectez-vous"
+            : type === "daily-already-ended"
+              ? "Vore journée de travail est déjà terminée, vous ne pouvez plus pointer"
+              : type === "weekly"
+                ? "Vous n'avez pas encore pointé votre arrivée cette semaine, pensez à le faire tous les jours"
+                : "La session n'est pas détectée, redémarrez l'application ou reconnectez-vous"
         }
         subtitleVariant="bodySmall"
         subtitleNumberOfLines={6}
