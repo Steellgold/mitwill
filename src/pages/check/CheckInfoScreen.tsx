73bd1193e6c7b3a125b6600e8c1abd925dc021c1
Gaëtan H <gaetanhus@gmail.com>
2024-03-05 21:13:54 +0100

    Update CheckInfoScreen component with additional card and styling

--- src/pages/check/CheckInfoScreen.tsx
+++ src/pages/check/CheckInfoScreen.tsx
@@ -1,10 +1,10 @@
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import type  { ReactElement } from "react";
 import { StyleSheet, View } from "react-native";
-import { Card, Text } from "react-native-paper";
+import { Avatar, Card, Divider, Text } from "react-native-paper";
 import type { RootStackParamList } from "../../../App";
 import { dayJS } from "../../lib/dayjs/day-js";
-import { calculateDiff, majdate } from "../../lib/dayjs/day-js.utils";
+import { calculateDiff } from "../../lib/dayjs/day-js.utils";
 import type { Diff } from "../../lib/dayjs/day-js.types";
 
 type Props = NativeStackScreenProps<RootStackParamList, "CheckInfoScreen">;
@@ -16,88 +16,92 @@ export const CheckInfoScreen = ({ route }: Props): ReactElement => {
 
   let duration: Diff = calculateDiff(route.params.start, dayJS().format());
   if (route.params.end) {
-    duration = calculateDiff(route.params.start, route.params.end);
+    duration = calculateDiff(route.params.start, route.params.end, true, "06:00", "14:00");
   }
 
   return (
     <View style={{ padding: 15 }}>
       <Card>
         <Card.Title
-          title={majdate(dayJS(route.params.date))}
-          subtitle="Informations détaillées"
+          style={{ marginTop: 10 }}
+          title="Pointage du jour"
+          subtitle="Données détaillées par rapport à votre pointage et votre temps de travail."
+          subtitleNumberOfLines={2}
           subtitleStyle={{ marginTop: -5 }}
         />
 
-        <Card.Content style={{ padding: 10 }}>
-          <Card>
-            <Card.Title title="votre journée" subtitle={""} subtitleStyle={{ marginTop: -5 }}/>
+        <View style={{ marginVertical: 5 }} />
 
-            <Card.Content>
-              <Text variant="bodySmall">Début de journée à: <Text style={styles.bold}>{dayJS(route.params.start).format("HH[h]mm[m]")}</Text></Text>
-              {route.params.end ? (
-                <Text variant="bodySmall">Fin de journée à: <Text style={styles.bold}>{dayJS(route.params.end).format("HH[h]mm[m]")}</Text></Text>
-              ) : (
-                <Text variant="bodySmall">Vous êtes actuellement entrain de travailler</Text>
-              )}
+        <Card.Content>
+          <View style={{ flexDirection: "row", gap: 10 }}>
+            <Card style={{ flex: 1 }}>
+              <Card.Title
+                title="Début de journée"
+                subtitle={dayJS(route.params.start).format("HH[h]mm[m]")} subtitleStyle={{ marginTop: -5 }}/>
+            </Card>
 
-              <Text variant="bodySmall">Durée de travail: <Text style={styles.bold}>{duration.hours}h{duration.minutes}m</Text></Text>
-            </Card.Content>
-          </Card>
-        </Card.Content>
-
-        {/* <View style={{ flexDirection: "row", gap: 10, margin: 10 }}>
-          <View style={{ flex: 1 }}>
-            <Card>
-              <Card.Title title="Début" subtitle={dayJS(route.params.start).format("HH[h]mm[m]")} subtitleStyle={{ marginTop: -5 }}/>
+            <Card style={{ flex: 1 }}>
+              <Card.Title
+                title="Fin de journée"
+                subtitle={dayJS(route.params.end).format("HH[h]mm[m]")} subtitleStyle={{ marginTop: -5 }}/>
             </Card>
           </View>
 
-          <View style={{ flex: 1 }}>
-            {route.params.end ? (
-              <Card>
-                <Card.Title title="Fin" subtitle={dayJS(route.params.end).format("HH[h]mm[m]")} subtitleStyle={{ marginTop: -5 }}/>
+          <View style={{ marginVertical: 5 }} />
+
+          <View style={{ flexDirection: "row", gap: 10 }}>
+            <Card style={{ flex: 1 }}>
+              <Card.Title
+                title="Heures travaillées"
+                subtitle={`${duration.hours} heures et ${duration.minutes} mins`} subtitleStyle={{ marginTop: -5 }}/>
+            </Card>
+
+            <Card style={{ flex: 1 }}>
+              <Card.Title
+                title="Pause"
+                subtitle={
+                  route.params.pauseTaken
+                    ? "45 mins"
+                    : "20 mins (oblg.)"
+                } subtitleStyle={{ marginTop: -5 }}/>
+            </Card>
+          </View>
+
+          <View style={{ marginVertical: 5 }} />
+
+          <View style={{ flexDirection: "row", gap: 10 }}>
+            {(parseInt(duration.nbrNights.hours) > 0) || (parseInt(duration.nbrNights.minutes) > 0) ? (
+              <Card style={{ flex: 1 }}>
+                <Card.Title
+                  title="Heures de nuit"
+                  subtitle={`${duration.nbrNights.hours} heures et ${duration.nbrNights.minutes} mins`} subtitleStyle={{ marginTop: -5 }}/>
               </Card>
             ) : (
-              <Card>
-                <Card.Title title="Fin" subtitle="En cours" subtitleStyle={{ marginTop: -5 }}/>
+              <Card style={{ flex: 1 }} disabled>
+                <Card.Title title="Heures de nuit" subtitle="Aucune" subtitleStyle={{ marginTop: -5 }}/>
+              </Card>
+            )}
+
+            {(parseInt(duration.nbrSupps.hours) > 0) || (parseInt(duration.nbrSupps.minutes) > 0) ? (
+              <Card style={{ flex: 1 }}>
+                <Card.Title
+                  title="Heures supps."
+                  subtitle={`${duration.nbrSupps.hours} heures et ${duration.nbrSupps.minutes} mins`} subtitleStyle={{ marginTop: -5 }}/>
+              </Card>
+            ) : (
+              <Card style={{ flex: 1 }} disabled>
+                <Card.Title title="Heures supps." subtitle="Aucune" subtitleStyle={{ marginTop: -5 }}/>
               </Card>
             )}
           </View>
+        </Card.Content>
 
-          <View style={{ flex: 1 }}>
-            <Card>
-              <Card.Title title="Durée" subtitle={`${duration.hours}h${duration.minutes}m`} subtitleStyle={{ marginTop: -5 }}/>
-            </Card>
-          </View>
+        <Divider style={{ marginTop: 15, marginBottom: 5 }}/>
+
+        <View style={{ alignItems: "center", flexDirection: "row", padding: 10, marginBottom: 5 }}>
+          <Avatar.Icon color="#fd7e46" size={24} icon="information" style={{ backgroundColor: "transparent" }}/>
+          <Text style={styles.orange}>Ces informations sont visibles par votre manager.</Text>
         </View>
-
-        <View style={{ flexDirection: "row", gap: 10, margin: 10, marginTop: -1 }}>
-          <View style={{ flex: 1 }}>
-            <Card>
-              <Card.Title title="Temps de pause" subtitle={
-                route.params.pauseTaken ? "45 minutes" : "20 minutes (obl.)"
-              } subtitleStyle={{ marginTop: -5 }}/>
-            </Card>
-          </View>
-
-          <View style={{ flex: 1 }}>
-            <Card>
-              <Card.Title title="Heures supp." subtitle={"0h"} subtitleStyle={{ marginTop: -5 }}/>
-            </Card>
-          </View>
-        </View>
-
-        <View style={{ flexDirection: "row", gap: 10, margin: 10, marginTop: -1 }}>
-          <View style={{ flex: 1 }}>
-            <Card>
-              <Card.Title title="Heures de nuits" subtitle={
-                (parseInt(duration.nbrNights.hours) > 0 || parseInt(duration.nbrNights.minutes) > 0)
-                  ? `${duration.nbrNights.hours}h${duration.nbrNights.minutes}m`
-                  : "Aucune heure de nuit"
-              } subtitleStyle={{ marginTop: -5 }}/>
-            </Card>
-          </View>
-        </View> */}
       </Card>
     </View>
   );
@@ -106,5 +110,8 @@ export const CheckInfoScreen = ({ route }: Props): ReactElement => {
 const styles = StyleSheet.create({
   bold: {
     fontWeight: "bold"
+  },
+  orange: {
+    color: "#fd7e46"
   }
 });
