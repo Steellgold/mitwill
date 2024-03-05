b3164a7644825e762fae1c0895db2d7f9d0c9fae
Gaëtan H <gaetanhus@gmail.com>
2024-03-06 00:12:37 +0100

    Add PlanningsScreen component

---
+++ src/pages/session/company/PlanningsScreen.tsx
@@ -0,0 +1,12 @@
+import type { ReactElement } from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+
+export const PlanningsScreen = (): ReactElement => {
+  return (
+    <View>
+      <Text>PlanningsScreen</Text>
+    </View>
+  );
+};
