64ea565aa621afed1bfffa448e836c1d77281305
Gaëtan H <gaetanhus@gmail.com>
2024-03-07 14:07:46 +0100

    Add IS_DEV constant to v.ts

--- v.ts
+++ v.ts
@@ -1,3 +1,4 @@
 export const VERSION = "0.1.0";
 export const RELEASE = false;
 export const NOTE_PLANNING = "Les plannings ne sont pas encore disponibles, les journées de travail sont donc de 06h00 à 14h00.";
+export const IS_DEV = false;
