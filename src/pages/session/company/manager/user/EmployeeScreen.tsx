6de12ca2199f2d4524f8ddb64f8cf57c96a5f95c
Gaëtan H <gaetanhus@gmail.com>
2024-03-11 02:37:58 +0100

    Add EmployeeScreen component

---
+++ src/pages/session/company/manager/user/EmployeeScreen.tsx
@@ -0,0 +1,11 @@
+import type { ReactElement } from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export const EmployeeScreen = (): ReactElement => {
+  return (
+    <View>
+      <Text>EmployeeScreen</Text>
+    </View>
+  );
+};
