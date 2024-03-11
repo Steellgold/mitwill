e618d5d666f217808fb88c4cd74be4044f3a34c0
Gaëtan H <gaetanhus@gmail.com>
2024-03-11 01:50:07 +0100

    Add fixTime function to handle time formatting

---
+++ src/lib/time.ts
@@ -0,0 +1,6 @@
+export const fixTime = (time: string | null): string => {
+  if (!time) return "00:00";
+
+  const [h, m] = time.split(":");
+  return `${h.length === 1 ? `0${h}` : h}:${m.length === 1 ? `${m}0` : m}`;
+};
