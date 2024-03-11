7dc3a11113d71c40cdbec4ebca2dbb9016615157
Gaëtan H <gaetanhus@gmail.com>
2024-03-11 21:36:20 +0100

    Update day-js types to include workTime and nbrNights

--- src/lib/dayjs/day-js.types.ts
+++ src/lib/dayjs/day-js.types.ts
@@ -3,15 +3,30 @@ export type Diff = {
   hours: string;
   minutes: string;
   seconds: string;
-  nbrSupps: {
-    hours: string;
-    minutes: string;
-  };
+  nbrSupps: TimeBeyond;
   nbrNights: {
     hours: string;
     minutes: string;
   };
 };
+
+export type DiffWithWT = {
+  days: string;
+  hours: string;
+  minutes: string;
+  seconds: string;
+  nbrSupps: TimeBeyond;
+  nbrNights: {
+    hours: string;
+    minutes: string;
+  };
+  workTime: TimeBeyond;
+};
+
+export type TimeBeyond = {
+  hours: string;
+  minutes: string;
+}
 
 export type DiffWithLong = {
   isMoreLonger: boolean;
