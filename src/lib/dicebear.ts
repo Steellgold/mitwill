0a771112f6420a122750c07ac6f1c6afcb57445f
Gaëtan H <gaetanhus@gmail.com>
2024-03-12 00:37:57 +0100

    Add avatarTime function to include timestamp in avatar URL

--- src/lib/dicebear.ts
+++ src/lib/dicebear.ts
@@ -1,3 +1,16 @@
+import { dayJS } from "./dayjs/day-js";
+
 export const getAvatar = (firstName: string, lastName: string): string => {
   return `https://api.dicebear.com/7.x/initials/png?seed=${firstName}+${lastName}&backgroundColor=fd7e46`;
+};
+
+export const avatarTime = (url: string | null): string => {
+  if (!url) return getAvatar("John", "Doe");
+
+  const dateWithTime = dayJS().format("YYYY-MM-DDTHH:mm:ss");
+  if (url.includes("?")) {
+    return `${url}&time=${dateWithTime}`;
+  }
+
+  return `${url}?time=${dateWithTime}`;
 };
