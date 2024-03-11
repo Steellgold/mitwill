8e062fb88c80a0ecaa04edc9f858a51778336812
Gaëtan H <gaetanhus@gmail.com>
2024-03-11 02:10:06 +0100

    Update avatar generation to use PNG format

--- src/lib/dicebear.ts
+++ src/lib/dicebear.ts
@@ -1,3 +1,3 @@
 export const getAvatar = (firstName: string, lastName: string): string => {
-  return `https://api.dicebear.com/7.x/initials/svg?seed=${firstName}+${lastName}&backgroundColor=fd7e46`;
+  return `https://api.dicebear.com/7.x/initials/png?seed=${firstName}+${lastName}&backgroundColor=fd7e46`;
 };
