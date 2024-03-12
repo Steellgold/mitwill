0518a33cb657613f5e6ef9738795513b6411cc95
Gaëtan H <gaetanhus@gmail.com>
2024-03-12 23:24:00 +0100

    Update version constant in v.ts

--- v.ts
+++ v.ts
@@ -1,4 +1,2 @@
-export const VERSION = "0.1.0";
-export const RELEASE = false;
-export const NOTE_PLANNING = "Les plannings ne sont pas encore disponibles, les journées de travail sont donc de 06h00 à 14h00.";
+export const APP_VERSION = "0.0.1";
 export const IS_DEV = false;
