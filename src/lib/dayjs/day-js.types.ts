814d35e6a236a446adc5ab78818e6fe06c12c071
Gaëtan H <gaetanhus@gmail.com>
2024-03-22 18:28:42 +0100

    Add PauseDiff type to day-js.types.ts

--- src/lib/dayjs/day-js.types.ts
+++ src/lib/dayjs/day-js.types.ts
@@ -17,6 +17,16 @@ export type SimpleDiff = {
   seconds: string;
 };
 
+export type PauseDiff = {
+  days: string;
+  hours: string;
+  minutes: string;
+  seconds: string;
+
+  nbr20Taken: number;
+  nbr45Taken: number;
+};
+
 export type DiffWithWT = {
   days: string;
   hours: string;
